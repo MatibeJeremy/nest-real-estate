078d4eba2a3e2b97d56ccce38f5e15387ef4e0f8
Jeremy Matibe <aomatibe@dtbafrica.com>
2025-08-25 21:50:23 +0300

    feat(swagger-docs): api documentation to see available endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // Swagger configurations
+  const config = new DocumentBuilder()
+    .setTitle('Real Estate Management')
+    .setDescription('A Nestjs API to manage real-estate projects')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 8000);
   console.log('Application is running on port:', process.env.PORT);
 }
